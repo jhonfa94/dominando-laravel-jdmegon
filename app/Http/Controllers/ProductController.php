4ceb13ceb6a19bfcedacadd991fd437527a2f14b
fabio.cardona <fabio.cardona@coytex.com.co>
2021-01-11 07:39:18 -0500

    Uso de datos, Manejo de informacion en Laravel

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,14 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
     public function index()
     {
         // return "This is list product from method index";
-        return view('products.index');
+        // $products = DB::table('products')->get();
+        // $products = Product::all();
+        // dd($products);
+        // return $products; # Retorna en la información en JSON
+        return view('products.index')->with([
+            'products' => Product::all()
+        ]);
     }
 
     public function create()
@@ -25,7 +33,17 @@ class ProductController extends Controller
     public function show($product)
     {
         // return "Showing product witch id {$product} from CONTROLLER";
-        return view('products.show');
+        // $product = DB::table('products')->where('id', '=', $product)->get();
+        // $product = DB::table('products')->where('id', '=', $product)->first();
+        // $product = DB::table('products')->find($product);
+        // $product = Product::find($product);
+        $product = Product::findOrFail($product);
+        // dd($product); # Depuramos el error
+
+        return view('products.show')->with([
+            'product' => $product,
+            'html' => '<h2>Html Jhonfa</html>'
+        ]);
     }
 
     public function edit($product)
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -10,8 +10,16 @@
 
 <body>
 
-    <h1>Soap(1)</h1>
-    <p>Best Soap ever</p>
+    {{-- {{ $product }} --}}
+
+
+    <h1>{{ $product->title }} ({{ $product->id }})</h1>
+    <p>{{ $product->description }}</p>
+    <p>{{ $product->price }}</p>
+    <p>{{ $product->stock }}</p>
+    <p>{{ $product->status }}</p>
+
+    {!!$html!!}
 
 </body>
 
