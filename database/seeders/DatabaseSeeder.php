4cd46c54c3dd728b117955407e307b56dee4e91f
fabio.cardona <fabio.cardona@coytex.com.co>
2021-01-14 18:12:41 -0500

    Generando usuarios, productos y ordenes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Order;
 use App\Models\Product;
+use App\Models\Payment;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
 
@@ -15,14 +17,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        \App\Models\User::create([            
+        $product = Product::factory(50)->create(); # Insertando 50 usuarios
+
+        \App\Models\User::create([
             'name' => 'Jhon Fabio Cardona',
             'email' => 'test@test.com',
             'email_verified_at' => now(),
-            'password' => bcrypt('12345678'), 
-            'remember_token' => Str::random(10),        
+            'password' => bcrypt('12345678'),
+            'remember_token' => Str::random(10),
         ]);
-        $product = Product::factory(50)->create();
+
+        $users =  \App\Models\User::factory(10)->create(); # Insertando 20 usuarios
+
+        # Insertando 10 ordenes
+        $orders = \App\Models\Order::factory(10)
+            ->make()
+            ->each(function ($order) use ($users) {
+                //Recorremos las ordenes
+                $order->customer_id =  $users->random()->id;
+                $order->save();
+
+                //Generamos el pago
+                $payment = Payment::factory()->make();
+                // $payment->order_id = $order->id;
+                // $payment->save();
+
+                $order->payment()->save($payment);
+            });
     }
 }
